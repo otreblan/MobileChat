49f0f53771ce1a9a1fe79e246d64caf7edf14a5d
otreblan <otreblain@gmail.com>
2020-06-29 11:51:31 -0500

    Up to date

--- app/src/main/java/com/example/mobilechat/MainActivity.java
+++ app/src/main/java/com/example/mobilechat/MainActivity.java
@@ -3,6 +3,21 @@ package com.example.mobilechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void onLoginClicked(View view) {
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText password = (EditText)findViewById(R.id.password);
+
+        String username_str = username.getText().toString();
+        String password_str = password.getText().toString();
+
+        Map<String, String> message = new HashMap<String, String>();
+
+        message.put("username", username_str);
+        message.put("password", password_str);
+
+        JSONObject jsonMessage = new JSONObject(message);
+
+        //showMessage(jsonMessage.toString());
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                // 10.0.2.2
+                "https://otreblan.ddns.net/authenticate",
+                jsonMessage,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject reponse) {
+                        showMessage("Ok");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        showMessage("No");
+                    }
+                }
+        );
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
     }
 }
