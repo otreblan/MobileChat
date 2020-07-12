c46b7fe8e6df8f75cc0e3409091cf88e281b5a4d
otreblan <otreblain@gmail.com>
2020-07-12 10:04:39 -0500

    Send message

--- app/src/main/java/com/example/mobilechat/ChatActivity.java
+++ app/src/main/java/com/example/mobilechat/ChatActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -14,9 +16,15 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -78,6 +86,50 @@ public class ChatActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         error.printStackTrace();
+                    }
+                }
+        );
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    public void onClickSend(View view) {
+        EditText send = (EditText)findViewById(R.id.send);
+
+        String send_str = send.getText().toString();
+        send.getText().clear();
+
+        Map<String, String> message = new HashMap<String, String>();
+
+        message.put("content", send_str);
+
+        JSONObject jsonMessage = new JSONObject(message);
+        try {
+            jsonMessage.put("user_from_id", userFromId);
+            jsonMessage.put("user_to_id", userToId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //showMessage(jsonMessage.toString());
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                // 10.0.2.2
+                "https://otreblan.ddns.net/messages",
+                jsonMessage,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //showMessage("Ok");
+                        getMessages();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //showMessage("No");
                     }
                 }
         );
